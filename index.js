304d9eea2e98d15c86d955a1f86dcc2f83143109
MarsilAL <marsil.alashkar@gmail.com>
2021-09-27 15:05:39 +0200

    add jquery to the project and style and make a ajax request

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hi from Console')
+const jquery = require('jquery')
+//
+$ = window.$ = window.jQuery = jquery;
+
+$(function () {
+    let endpoint = 'https://jsonplaceholder.typicode.com/todos'
+    $.ajax({
+        url: endpoint,
+        contentType: "application/json",
+        dataType: 'json',
+        success: function (result) {
+            for(let x = 0; x <result.length; x++){
+
+            }
+            // First Object
+            var id = document.getElementById("todoID")
+            id.innerHTML = 'ID: ' + result[3].id
+
+            var title = document.getElementById("todoTitle")
+            title.innerHTML = 'Title: ' + result[3].title
+
+            var isCompleted = document.getElementById("isC")
+            isCompleted.innerHTML = 'is Completed: ' + result[3].completed
+            
+        }
+    })
+
+})
